58cec55bb21cf6c0ffe11432272ee19a023a4e1b
ryoux <s162951@stud.hioa.no>
2015-05-25 15:50:33 +0200

    Create simple_database_fetch.js

---
+++ simple_database_fetch.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+
+#Hent events fra database med AngularJS
+#Og setter dem inn i et vanlig JS array
+
+var events = ['Event1','Event2','Event3','Event4','Event5'];
+events.sort();
+var eventsCount = events.length;
+
+for(var i=0; i<=eventsCount; i++){
+document.write(events[i] + ('<br />'));
+}
+
+</script>
